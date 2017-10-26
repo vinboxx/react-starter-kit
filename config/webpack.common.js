5e2b75cc96d855959241ed11b52a49f1870909fd
Weerapat Poosri <vinboxx@gmail.com>
2017-10-26 21:26:05 +0700

    Initial commit

---
+++ config/webpack.common.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const appPath = path.resolve(__dirname, '../');
+
+module.exports = {
+    entry: {
+        app: path.resolve(appPath, 'src/index.js')
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist'], {
+            root: appPath
+        }),
+        new HtmlWebpackPlugin({
+            title: 'React App',
+            template: path.resolve(appPath, 'src/index.html')
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(appPath, 'dist')
+    },
+    target: 'web',
+    stats: 'normal',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: [
+                    /node_modules/
+                ],
+                use: 'babel-loader'
+            }
+        ]
+    }
+};
